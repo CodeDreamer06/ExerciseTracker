9e574ef4456f2668303e9a0bfb3ce580272bdc22
Abhinav <codedreamer06@gmail.com>
2022-06-27 21:11:19 +0530

    Implemented repository pattern

--- Program.cs
+++ Program.cs
@@ -4,38 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        var command = "";
-        var service = new ExerciseService();
-        const string help = @"
-# Exercise Tracker
-  A simple exercise logger for you to measure your progress!
-* exit or 0: stop the program
-* show: display existing logs
-* add: create an exercise log
-* update [id]: change an existing log
-* remove [optional: id]: delete the last recent log
-Use help to display this message again!
-";
+        var service = new WorkoutService();
+        Console.WriteLine(Helpers.MainMessage);
 
         while (true)
         {
-            command = Console.ReadLine()!.ToLower();
+            var rawCommand = Console.ReadLine()!;
+            var command = rawCommand.ToLower().Trim();
 
             if (command is "exit" or "0") break;
 
-            else if (command is "help") Console.WriteLine(help);
+            else if (command is "help") Console.WriteLine(Helpers.MainMessage);
 
             else if (string.IsNullOrWhiteSpace(command)) continue;
 
             else if (command.StartsWith("add")) service.Add();
-
+            
             else if (command.StartsWith("show")) service.Show();
 
-            else if (command.StartsWith("update")) service.Update(0);
+            else if (command.StartsWith("update")) 
+                service.Update(rawCommand.GetNumber("update"));
 
-            else if (command.StartsWith("remove")) service.Remove(0);
+            else if (command.StartsWith("remove")) 
+                service.Remove(rawCommand.GetNumber("remove"));
 
-            else Console.WriteLine("Not a command. Use 'help' if required. ");
+            else
+            {
+                string suggestion = Helpers.CorrectSpelling(command);
+                Console.WriteLine($"Not a command. Use 'help' if required. {suggestion}");
+            }
         }
     }
 }
